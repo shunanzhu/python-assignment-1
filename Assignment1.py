91419c8e0c428d1625e3491ed92bd51c27e27613
shunanzhu <shunanzhu@gmail.com>
2022-09-03 18:46:13 -0700

    python assignment 1

---
+++ Assignment1.py
@@ -0,0 +1,34 @@
+"""
+Assignment 1: Opening Lines
+Submitted by Shunan Zhu
+Submitted: April 10, 2022
+
+Assignment 1: This program demonstrates printing lines of text to the screen
+"""
+
+
+def print_header():
+    """
+    Print a header.
+    """
+    print("STEM Center Temperature Project")
+    print("Shunan Zhu")
+
+
+def main():
+    """
+    Call print_header() function.
+    """
+    print_header()
+
+
+if __name__ == "__main__":
+    main()
+
+"""
+/Users/shunanzhu/PycharmProjects/Assignment1/venv/bin/python /Users/shunanzhu/PycharmProjects/Assignment1/Assignment1.py
+STEM Center Temperature Project
+Shunan Zhu
+
+Process finished with exit code 0
+"""
